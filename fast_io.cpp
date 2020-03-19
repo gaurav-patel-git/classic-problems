3d0676c771e6839681498896367ad2809be02c21
Gaurav Patel <34380366+gaurav-patel-git@users.noreply.github.com>
2020-03-19 17:10:17 +0530

    Create fast_io.cpp

---
+++ fast_io.cpp
@@ -0,0 +1,12 @@
+// A fast IO program 
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int main() 
+{ 
+	// added the two lines below 
+	ios_base::sync_with_stdio(false); 
+	cin.tie(NULL); 
+	
+	// your code goes here
+} 
