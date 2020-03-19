91941fcadeb6c690f1611bd0844120528b36ea9e
Gaurav Patel <34380366+gaurav-patel-git@users.noreply.github.com>
2020-03-19 17:07:42 +0530

    Update coin_change.py

--- coin_change.py
+++ coin_change.py
@@ -4,11 +4,11 @@
 
 
 def coin_change(coins, target):
-	inf = 10**9
-	dp = [0] + [ inf for i in range(target)]
+	inf = 10**9	
+	dp = [0] + [ inf for i in range(target)]  # target 0 can be acchived using 0 coin and rest are infiniti for now
 	for coin in coins:
 		for i in range(coin, target+1):
-			dp[i] = min(dp[i], dp[i-coin]+1)
+			dp[i] = min(dp[i], dp[i-coin]+1)  # formula for dp
     if dp[target]!=10**9: return dp[target]
 	else: return "Impossible"
 
